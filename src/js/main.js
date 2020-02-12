e467cd289563d88f221243dc28bcdacf16c32e36
Sorry <tryelseb@gmail.com>
2020-02-13 02:14:56 +0300

    full HOME page

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,31 @@
 //= ../../node_modules/foundation-sites/dist/js/foundation.min.js
 //= partials/jquery.validate.min.js
 
-$( document ).ready(function() {
+$(document).ready(function () {
   // Category mobile menu
-  $('#category-mob').click(function() {
+  $('#category-mob').click(function () {
     $('.category__list').animate({
       height: 'toggle',
       'margin-bottom': '1em'
     }, 800)
   });
   // End category mobile menu
+  $("#mobile-btn").on( "click", function() {
+    if ($(".menu").hasClass('show-menu')) {
+      $(".menu").animate({
+        width: '0'
+      }, 50, function() { 
+        $(".menu").removeClass('show-menu'); 
+        $(".menu__list").removeAttr('style');
+        $(".menu").removeAttr('style');
+      });      
+    } else {
+      $(".menu").addClass('show-menu').animate({
+        width: "100%"
+      }, 50);
+      $(".menu__list").animate({
+        width: "65%"
+      }, 100);
+    }
+  });
 });
